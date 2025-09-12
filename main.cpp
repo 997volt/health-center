38efacf48623fa93b687956e52747d96369fcbce
user <phgit@pm.me>
2025-09-12 08:40:19 +0200

    remove old not used functions

--- main.cpp
+++ main.cpp
@@ -20,6 +20,8 @@ const int MAX_CALORIES_DIFF_IN_RUN = 600;
 const int DAYS_TO_ANALYSE = 10;
 
 bool is_date_after(tm tocheck, tm relative_date);
+bool is_date_same(tm tocheck, tm relative_date);
+ifstream open_wl_file(string path);
 
 class WaistlineData {
 private:
@@ -100,103 +102,6 @@ public:
 
     float get_weight(){
         return weight;
-    }
-};
-
-// container for averaged weekly data
-class WeeklyData {
-private:
-    tm date; //last day of data
-    float weight;
-    float calories;
-
-public:
-    WeeklyData(vector<WaistlineData> &wldata, tm date_done){
-        int weight_datapoints = 0;
-        int cal_datapoints = 0;
-        weight = 0;
-        calories = 0;
-        for (int i = 0; i < wldata.size(); i++) {
-            tm dataset_date = wldata[i].get_tm_date();
-            if (is_date_after(dataset_date, date_done)){
-                float dataset_weight = wldata[i].get_weight();
-                if (dataset_weight != 0) {
-                    weight = weight + dataset_weight;
-                    weight_datapoints++;
-                }
-                float dataset_calories = float(wldata[i].get_calories());
-                if (dataset_calories != 0) {
-                    calories = calories + dataset_calories;
-                    cal_datapoints++;
-                }
-                if (dataset_date.tm_wday == DAY_OF_WEEK_FOR_AVERAGES
-                    || i == wldata.size()-1) {
-                    weight = weight / weight_datapoints;
-                    calories = calories / cal_datapoints;
-                    date = dataset_date;
-                    return;
-                }
-            }
-        }
-    }
-
-    tm get_date(){
-        return date;
-    }
-
-    float get_calories(){
-        return calories;
-    }
-
-    float get_weight(){
-        return weight;
-    }
-
-    void printdata(){
-        cout << "date: " << date.tm_mday << "-" << date.tm_mon+1 << "-" << date.tm_year+1900 
-        << ",  calories: " << calories 
-        << ",  weight: " << weight 
-        << endl;
-    }
-};
-
-class WeightAnalisysData {
-private:
-    float avg_weight;
-    float weight_diff_kg;
-    float weight_diff_precentage;
-
-    public:
-    WeightAnalisysData(float _avg_weight, float _weight_diff_kg, float _weight_diff_precentage){
-        avg_weight = _avg_weight;
-        weight_diff_kg = _weight_diff_kg;
-        weight_diff_precentage = _weight_diff_precentage;
-    }
-
-    WeightAnalisysData(float _avg_weight, float previous_weight){
-        
-        avg_weight = _avg_weight;
-        if (previous_weight != 0) {
-            weight_diff_kg = avg_weight - previous_weight;
-            weight_diff_precentage = 100 * weight_diff_kg / avg_weight;
-        }
-        else {
-            weight_diff_kg = 0;
-            weight_diff_precentage = 0;            
-        }
-    }
-
-    WeightAnalisysData(float _avg_weight){
-        avg_weight = _avg_weight;
-        weight_diff_kg = 0;
-        weight_diff_precentage = 0;
-    }
-
-    float get_avg_weight(){
-        return avg_weight;
-    }
-    float get_weight_diff_precentage(){
-        return weight_diff_precentage;
     }
 };
 
@@ -304,63 +209,6 @@ void read_wl_data(string path, vector<WaistlineData> &wldata){
         }
     }
     file.close();
-}
-
-void calculate_weekly_data(vector<WaistlineData> &wldata, vector<WeeklyData> &weekly_data){
-    tm date_done = {};
-    bool exit = false;
-    do {
-        WeeklyData calc_data = WeeklyData(wldata, date_done);
-        date_done = calc_data.get_date();
-        weekly_data.push_back(calc_data);
-        exit = is_date_same(wldata[wldata.size()-1].get_tm_date(), date_done);
-    }while (!exit);
-}
-
-void get_avg_weights(vector<WaistlineData> &wldata, vector<WeightAnalisysData> &avg_weight_vector, int num_of_weeks){
-    float combined_weight = 0;
-    int weight_datapoints = 0;
-    float avg_weight = 0;
-    float previous_weight = 0;
-    for (int day = 0; day < num_of_weeks*7; day++){
-        int data_index = wldata.size() - day - 1;
-        float wlweight = wldata[data_index].get_weight();
-        if (wlweight != 0){
-            combined_weight += wlweight;
-            weight_datapoints += 1;
-        }
-        if (day % 7 == 6) {
-            avg_weight = combined_weight/weight_datapoints;
-            avg_weight_vector.push_back(WeightAnalisysData(avg_weight, previous_weight));
-            previous_weight = avg_weight;
-            combined_weight = 0;
-            weight_datapoints = 0;
-        }
-    }
-}
-
-void analyse_last_weeks(vector<WaistlineData> &wldata){
-    vector<WeightAnalisysData> avg_weight;
-    get_avg_weights(wldata, avg_weight, 3);
-    for (int i = 0; i < avg_weight.size(); i++){
-        cout << "avg_weight[" << i << "] = " << avg_weight[i].get_avg_weight() << endl;
-        cout << "weight_diff_precentage[" << i << "] = " << avg_weight[i].get_weight_diff_precentage() << endl;
-    }
-}
-
-int last_run_count(vector<WeeklyData> &weekly_data){
-    int num_of_items = weekly_data.size();
-    int num_of_counted = 1;
-    float avg_calories = weekly_data[num_of_items-1].get_calories(); 
-    for (int i = num_of_items-2; i > 0; i--) {
-        float this_item = weekly_data[i].get_calories();
-        if (abs(this_item - avg_calories) > MAX_CALORIES_DIFF_IN_RUN){
-            break;
-        }
-        avg_calories = (avg_calories*num_of_counted + this_item)/(num_of_counted+1); 
-        num_of_counted++;
-    }
-    return num_of_counted;
 }
 
 void analyse_regression(float coefficent){
